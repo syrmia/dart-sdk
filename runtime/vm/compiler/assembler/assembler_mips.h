e1713aae58f44df1da9e1e983370fa6ace83d86e
Andrea Milinkovic <andrea.milinkovic@htecgroup.com>
2025-08-11 12:07:08 +0200

    Add MIPS Assembler class with instruction encoding methods
    Add a new Assembler class for MIPS in the Dart VM backend.
    Supports emitting and encoding common instruction types.
    Also add enums for MIPS opcodes and functions needed for encoding.

--- runtime/vm/compiler/assembler/assembler_mips.h
+++ runtime/vm/compiler/assembler/assembler_mips.h
@@ -5,14 +5,19 @@
 #ifndef RUNTIME_VM_ASSEMBLER_MIPS_H_
 #define RUNTIME_VM_ASSEMBLER_MIPS_H_
 
+#include "vm/compiler/assembler/assembler_base.h"
 #include "vm/constants.h"
 
 // References to documentation in this file refer to:
 // "MIPS® Architecture For Programmers Volume I-A:
 //   Introduction to the MIPS32® Architecture" in short "VolI-A"
+//   available at:
+//   https://s3-eu-west-1.amazonaws.com/downloads-mips/documents/MD00082-2B-MIPS32INT-AFP-06.01.pdf
 // and
 // "MIPS® Architecture For Programmers Volume II-A:
 //   The MIPS32® Instruction Set" in short "VolII-A"
+//   available at:
+//   https://s3-eu-west-1.amazonaws.com/downloads-mips/documents/MD00086-2B-MIPS32BIS-AFP-6.06.pdf
 namespace dart {
 namespace compiler {
 
@@ -79,6 +84,79 @@ class FieldAddress : public Address {
   FieldAddress& operator=(const FieldAddress& other) {
     Address::operator=(other);
     return *this;
+  }
+};
+
+class Assembler : public AssemblerBase {
+ public:
+  explicit Assembler(ObjectPoolBuilder* object_pool_builder,
+                     intptr_t far_branch_level = 0)
+      : AssemblerBase(object_pool_builder),
+        use_far_branches_(far_branch_level != 0),
+        delay_slot_available_(false),
+        in_delay_slot_(false),
+        constant_pool_allowed_(false) {
+          UNIMPLEMENTED();
+  }
+  ~Assembler() {}
+  
+ private:
+  bool use_far_branches_;
+  bool delay_slot_available_;
+  bool in_delay_slot_;
+
+  bool constant_pool_allowed_;
+
+  void Emit(int32_t value) {
+    // Emitting an instruction clears the delay slot state.
+    in_delay_slot_ = false;
+    delay_slot_available_ = false;
+    AssemblerBuffer::EnsureCapacity ensured(&buffer_);
+    buffer_.Emit<int32_t>(value);
+  }
+
+// Encode CPU instructions according to the types specified in
+// Figures 5‑1, 5‑3, and 5‑8 in
+// MIPS® Architecture For Programmers Volume I‑A: Introduction to the MIPS32® Architecture, Revision 6.01.
+// Available at:
+// https://s3-eu-west-1.amazonaws.com/downloads-mips/documents/MD00082-2B-MIPS32INT-AFP-06.01.pdf
+  void EmitIType(Opcode opcode, Register rs, Register rt, uint16_t imm) {
+    Emit(opcode << kOpcodeShift | rs << kRsShift | rt << kRtShift | imm);
+  }
+
+  void EmitLoadStore(Opcode opcode, Register rt, const Address& addr) {
+    Emit(opcode << kOpcodeShift | rt << kRtShift | addr.encoding());
+  }
+
+  void EmitFpuLoadStore(Opcode opcode, FRegister ft, const Address& addr) {
+    Emit(opcode << kOpcodeShift | ft << kFtShift | addr.encoding());
+  }
+
+  void EmitRegImmType(Opcode opcode, Register rs, RtRegImm code, uint16_t imm) {
+    Emit(opcode << kOpcodeShift | rs << kRsShift | code << kRtShift | imm);
+  }
+
+  void EmitJType(Opcode opcode, uint32_t destination) { UNIMPLEMENTED(); }
+
+  void EmitRType(Opcode opcode,
+                 Register rs,
+                 Register rt,
+                 Register rd,
+                 int sa,
+                 SpecialFunction func) {
+    ASSERT(Utils::IsUint(5, sa));
+    Emit(opcode << kOpcodeShift | rs << kRsShift | rt << kRtShift |
+         rd << kRdShift | sa << kSaShift | func << kFunctionShift);
+  }
+
+  void EmitFpuRType(Opcode opcode,
+                    Format fmt,
+                    FRegister ft,
+                    FRegister fs,
+                    FRegister fd,
+                    Cop1Function func) {
+    Emit(opcode << kOpcodeShift | fmt << kFmtShift | ft << kFtShift |
+         fs << kFsShift | fd << kFdShift | func << kCop1FnShift);
   }
 };
 
