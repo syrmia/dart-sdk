7b0726b7525301d15f9ebf09350b600724388145
Aleksandar Djajic <aleksandar.djajic@htecgroup.com>
2026-03-09 10:18:58 +0100

    [mips] Report mips in Dart::FeaturesString
    Add TARGET_ARCH_MIPS handling in Dart::FeaturesString
    so the runtime reports "mips" in the architecture
    features string.

--- runtime/vm/dart.cc
+++ runtime/vm/dart.cc
@@ -1146,6 +1146,8 @@ char* Dart::FeaturesString(IsolateGroup* isolate_group,
     buffer.AddString(" riscv32");
 #elif defined(TARGET_ARCH_RISCV64)
     buffer.AddString(" riscv64");
+#elif defined(TARGET_ARCH_MIPS)
+    buffer.AddString(" mips");
 #else
 #error What architecture?
 #endif
