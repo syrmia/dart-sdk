7b5441dd956d79faa2ab190dea453dcf0742a2c0
Aleksandar Djajic <aleksandar.djajic@htecgroup.com>
2026-03-12 09:30:16 +0100

    [mips] Implement FlowGraphCompiler stub call emission
    Add MIPS-specific implementation of FlowGraphCompiler methods
    for emitting stub calls and stub tail calls.
    EmitCallToStub performs a pc-relative call when possible and
    falls back to JumpAndLink when a pc-relative call cannot be
    used.
    EmitTailCallToStub performs a pc-relative tail call when
    possible. Otherwise it loads the stub location into
    CODE_REG and jumps to its entry point using entry_point_offset.

---
+++ runtime/vm/compiler/backend/flow_graph_compiler_mips.cc
@@ -0,0 +1,57 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#include "vm/globals.h"  // Needed here to get TARGET_ARCH_MIPS.
+#if defined(TARGET_ARCH_MIPS)
+
+#include "vm/compiler/backend/flow_graph_compiler.h"
+
+#include "vm/compiler/backend/il_printer.h"
+
+namespace dart {
+
+#define __ assembler()->
+
+void FlowGraphCompiler::EmitCallToStub(
+    const Code& stub,
+    ObjectPool::SnapshotBehavior snapshot_behavior) {
+  ASSERT(!stub.IsNull());
+  if (CanPcRelativeCall(stub)) {
+    __ GenerateUnRelocatedPcRelativeCall();
+    AddPcRelativeCallStubTarget(stub);
+  } else {
+    __ JumpAndLink(stub, compiler::ObjectPoolBuilderEntry::kNotPatchable,
+                   CodeEntryKind::kNormal, snapshot_behavior);
+    AddStubCallTarget(stub);
+  }
+}
+
+void FlowGraphCompiler::EmitTailCallToStub(const Code& stub) {
+  ASSERT(!stub.IsNull());
+  if (CanPcRelativeCall(stub)) {
+    if (flow_graph().graph_entry()->NeedsFrame()) {
+      __ LeaveDartFrame();
+    }
+    __ GenerateUnRelocatedPcRelativeTailCall();
+    AddPcRelativeTailCallStubTarget(stub);
+#if defined(DEBUG)
+    __ Breakpoint();
+#endif
+  } else {
+    __ LoadObject(CODE_REG, stub);
+    if (flow_graph().graph_entry()->NeedsFrame()) {
+      __ LeaveDartFrame();
+    }
+    __ lw(TMP, compiler::FieldAddress(
+                   CODE_REG, compiler::target::Code::entry_point_offset()));
+    __ jr(TMP);
+    AddStubCallTarget(stub);
+  }
+}
+
+#undef __
+
+}  // namespace dart
+
+#endif  // defined(TARGET_ARCH_MIPS)
