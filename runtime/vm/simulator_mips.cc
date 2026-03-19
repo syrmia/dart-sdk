fc3c2f2bc1e69b16ceaf1bbe637ad643c54c4fd8
Slavisa Petrovic <slavisa.petrovic@htecgroup.com>
2026-03-19 09:20:29 +0100

    [mips] Implement initial Simulator support
    Add the initial Simulator class declaration along with
    implementation of Init(), the constructor/destructor,
    Current(), set_register(), get_register(), and
    JumpToFrame().

--- runtime/vm/simulator_mips.cc
+++ runtime/vm/simulator_mips.cc
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+#include <setjmp.h>  // NOLINT
+#include <stdlib.h>
+
 #include "vm/globals.h"
 #if defined(TARGET_ARCH_MIPS)
 
@@ -10,12 +13,164 @@
 
 #include "vm/simulator.h"
 
+#include "vm/constants.h"
+#include "vm/isolate.h"
+#include "vm/os_thread.h"
+#include "vm/stack_frame.h"
+#include "vm/stack_resource.h"
+
 namespace dart {
 
+// SimulatorSetjmpBuffer are linked together, and the last created one
+// is referenced by the Simulator. When an exception is thrown, the exception
+// runtime looks at where to jump and finds the corresponding
+// SimulatorSetjmpBuffer based on the stack pointer of the exception handler.
+// The runtime then does a Longjmp on that buffer to return to the simulator.
+class SimulatorSetjmpBuffer {
+ public:
+  void Longjmp() {
+    // "This" is now the last setjmp buffer.
+    simulator_->set_last_setjmp_buffer(this);
+    longjmp(buffer_, 1);
+  }
 
+  explicit SimulatorSetjmpBuffer(Simulator* sim) {
+    simulator_ = sim;
+    link_ = sim->last_setjmp_buffer();
+    sim->set_last_setjmp_buffer(this);
+    sp_ = static_cast<uword>(sim->get_register(SP));
+  }
+
+  ~SimulatorSetjmpBuffer() {
+    ASSERT(simulator_->last_setjmp_buffer() == this);
+    simulator_->set_last_setjmp_buffer(link_);
+  }
+
+  SimulatorSetjmpBuffer* link() { return link_; }
+
+  uword sp() { return sp_; }
+
+ private:
+  uword sp_;
+  Simulator* simulator_;
+  SimulatorSetjmpBuffer* link_;
+  jmp_buf buffer_;
+
+  friend class Simulator;
+};
+
+void Simulator::Init() {}
+
+Simulator::Simulator() {
+  // Setup simulator support first. Some of this information is needed to
+  // setup the architecture state.
+  // We allocate the stack here, the size is computed as the sum of
+  // the size specified by the user and the buffer space needed for
+  // handling stack overflow exceptions. To be safe in potential
+  // stack underflows we also add some underflow buffer space.
+  stack_ =
+      new char[(OSThread::GetSpecifiedStackSize() + OSThread::kStackSizeBufferMax +
+                kSimulatorStackUnderflowSize)];
+  // Low address.
+  stack_limit_ = reinterpret_cast<uword>(stack_);
+    // Limit for StackOverflowError.
+  overflow_stack_limit_ = stack_limit_ + OSThread::kStackSizeBufferMax;
+  // High address.
+  stack_base_ = overflow_stack_limit_ + OSThread::GetSpecifiedStackSize();
+
+  icount_ = 0;
+  Random random_;
+  delay_slot_ = false;
+  break_pc_ = NULL;
+  break_instr_ = 0;
+  last_setjmp_buffer_ = NULL;
+  top_exit_frame_info_ = 0;
+
+  // Setup architecture state.
+  // All registers are initialized to zero to start with.
+  for (int i = 0; i < kNumberOfCpuRegisters; i++) {
+    registers_[i] = 0;
+  }
+  pc_ = 0;
+  // The sp is initialized to point to the bottom (high address) of the
+  // allocated stack area.
+  registers_[SP] = stack_base_;
+
+  // All double-precision registers are initialized to zero.
+  for (int i = 0; i < kNumberOfFRegisters; i++) {
+    fregisters_[i] = 0.0;
+  }
+  fcsr_ = 0;
+}
+
+Simulator::~Simulator() {
+  delete[] stack_;
+  Isolate* isolate = Isolate::Current();
+  if (isolate != NULL) {
+    isolate->set_simulator(NULL);
+  }
+}
+
+// Get the active Simulator for the current isolate.
+Simulator* Simulator::Current() {
+  Simulator* simulator = Isolate::Current()->simulator();
+  if (simulator == NULL) {
+    simulator = new Simulator();
+    Isolate::Current()->set_simulator(simulator);
+  }
+  return simulator;
+}
+
+// Sets the register in the architecture state.
+void Simulator::set_register(Register reg, int32_t value) {
+  if (reg != R0) {
+    registers_[reg] = value;
+  }
+}
+
+// Get the register from the architecture state.
+int32_t Simulator::get_register(Register reg) const {
+  if (reg == R0) {
+    return 0;
+  }
+  return registers_[reg];
+}
+
+void Simulator::JumpToFrame(uword pc, uword sp, uword fp, Thread* thread) {
+  // Walk over all setjmp buffers (simulated --> C++ transitions)
+  // and try to find the setjmp associated with the simulated stack pointer.
+  SimulatorSetjmpBuffer* buf = last_setjmp_buffer();
+  while (buf->link() != NULL && buf->link()->sp() <= sp) {
+    buf = buf->link();
+  }
+  ASSERT(buf != NULL);
+
+  // The C++ caller has not cleaned up the stack memory of C++ frames.
+  // Prepare for unwinding frames by destroying all the stack resources
+  // in the previous C++ frames.
+  StackResource::Unwind(thread);
+
+  // Unwind the C++ stack and continue simulation in the target frame.
+  set_pc(static_cast<int32_t>(pc));
+  set_register(SP, static_cast<int32_t>(sp));
+  set_register(FP, static_cast<int32_t>(fp));
+  set_register(THR, reinterpret_cast<int32_t>(thread));
+  // Set the tag.
+  thread->set_vm_tag(VMTag::kDartTagId);
+  // Clear top exit frame.
+  thread->set_top_exit_frame_info(0);
+  // Restore pool pointer.
+  int32_t code =
+      *reinterpret_cast<int32_t*>(fp + kPcMarkerSlotFromFp * kWordSize);
+  int32_t pp = *reinterpret_cast<int32_t*>(code + Code::object_pool_offset() -
+                                           kHeapObjectTag);
+  set_register(CODE_REG, code);
+  set_register(PP, pp);
+  buf->Longjmp();
+}
 
 }  // namespace dart
 
 #endif  // defined(USING_SIMULATOR)
 
-#endif  // defined TARGET_ARCH_MIPS
+#endif  // defined(TARGET_ARCH_MIPS)
--- runtime/vm/simulator_mips.h
+++ runtime/vm/simulator_mips.h
@@ -2,6 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// Declares a Simulator for MIPS instructions if we are not generating a native
+// MIPS binary. This Simulator allows us to run and debug MIPS code generation
+// on regular desktop machines.
+// Dart calls into generated code by "calling" the InvokeDartCode stub,
+// which will start execution in the Simulator or forwards to the real entry
+// on a MIPS HW platform.
 
 #ifndef RUNTIME_VM_SIMULATOR_MIPS_H_
 #define RUNTIME_VM_SIMULATOR_MIPS_H_
@@ -10,14 +16,74 @@
 #error Do not include simulator_mips.h directly; use simulator.h.
 #endif
 
+#include "vm/constants.h"
+#include "vm/random.h"
+
 namespace dart {
 
+class Isolate;
+class SimulatorSetjmpBuffer;
+class Thread;
+
 class Simulator {
-    // The files simulator_mips.h, simulator_mips.cc and the empty class Simulator
-    // inside it were added only so that it would be possible to run the command
-    // `./tools/run_offsets_extractor.dart`,
-    // in order to obtain offsets for the MIPS architecture.
-    // The implementation of this class will be added later.
+ public:
+  static constexpr uword kSimulatorStackUnderflowSize = 64;
+
+  Simulator();
+  ~Simulator();
+
+  // The currently executing Simulator instance, which is associated to the
+  // current isolate.
+  static Simulator* Current();
+
+  // Accessors for register state.
+  void set_register(Register reg, int32_t value);
+  int32_t get_register(Register reg) const;
+
+  // Accessor for the pc.
+  void set_pc(int32_t value) { pc_ = value; }
+
+  // Call on program start.
+  static void Init();
+
+  void JumpToFrame(uword pc, uword sp, uword fp, Thread* thread);
+
+ private:
+  // Special registers for the results of div, divu.
+  int32_t hi_reg_;
+  int32_t lo_reg_;
+
+  int32_t registers_[kNumberOfCpuRegisters];
+  int32_t fregisters_[kNumberOfFRegisters];
+  int32_t fcsr_;
+  int32_t pc_;
+
+  // Simulator support.
+  char* stack_;
+  uword stack_limit_;
+  uword overflow_stack_limit_;
+  uword stack_base_;
+  uint64_t icount_;
+  bool delay_slot_;
+  SimulatorSetjmpBuffer* last_setjmp_buffer_;
+  Random random_;
+
+  // Registered breakpoints.
+  Instr* break_pc_;
+  int32_t break_instr_;
+
+  // Exclusive access reservation.
+  uword exclusive_access_addr_;
+  uword exclusive_access_value_;
+
+  // Longjmp support for exceptions.
+  SimulatorSetjmpBuffer* last_setjmp_buffer() { return last_setjmp_buffer_; }
+  void set_last_setjmp_buffer(SimulatorSetjmpBuffer* buffer) {
+    last_setjmp_buffer_ = buffer;
+  }
+
+  friend class SimulatorSetjmpBuffer;
+  DISALLOW_COPY_AND_ASSIGN(Simulator);
 };
 
 }  // namespace dart
